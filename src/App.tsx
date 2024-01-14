ad8848fe23b1b5eef87377d97035b39373a4c850
Begon <japgo@naver.com>
2024-01-14 23:14:01 +0900

    add: front end 기본 구조 작성.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import TopNav from './TopNav';
+import Home from './pages/Home';
+import News from './pages/News';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <TopNav/>
+            <Routes>
+                <Route path='/' Component={Home} />
+                <Route path='/news' Component={News} />
+                <Route path='/*' element={'찾을 수 없는 페이지 입니다.'} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
