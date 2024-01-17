25d083efb8c46983c0f334e4c3bfe4f405755ff7
Begon <japgo@naver.com>
2024-01-18 01:08:19 +0900

    style: 코드 정리

--- src/dto/UserDto.ts
+++ src/dto/UserDto.ts
@@ -3,12 +3,11 @@ export interface UserDto {
 	introduction: string;
 	profile_url: string;
 	guestBookList: GuestBookDto[];
-  }
+}
   
 export interface GuestBookDto {
-	// GuestBookDto의 필드에 대한 타입을 정의하세요
 	id: number;
 	content: string;
 	nickname: string;
 	createdAt: string;
-  }
+}
