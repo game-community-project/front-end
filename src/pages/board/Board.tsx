86ee80d76655b42ac955653d927d66ab9e3416f9
oneseel <seno10065@gmail.com>
2024-01-17 21:01:05 +0900

    feat : 페이징 처리 및 css 추가

--- src/pages/board/Board.tsx
+++ src/pages/board/Board.tsx
@@ -1,34 +1,87 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { BoardDto } from "../../dto/Board";
-import { Col, Row } from "react-bootstrap";
+import { Container, Row, Col, Table } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import "./Board.css"; // Import your custom CSS file
 
 const Board: React.FC = () => {
   const [board, setBoard] = useState<Array<BoardDto>>([]);
 
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
+
   useEffect(() => {
     getBoard();
   }, []);
- 
-  const getBoard = async () => {
-    const res  = await axios.get('http://localhost:8080/api/posts?type=PC_GAME&game=LEAGUE_OF_LEGEND&board=FREE_BOARD&page=1&size=10&sortKey=createdAt&isAsc=false');
-    console.log(res);
-    setBoard(res.data.data.content);
-  }
- 
+
+  const getBoard = async (page = 1) => {
+    try {
+      const res = await axios.get(
+        `http://51.21.48.160:8080/api/posts?type=PC_GAME&game=LEAGUE_OF_LEGEND&board=FREE_BOARD&page=${page}&size=10&sortKey=createdAt&isAsc=false`
+      );
+      console.log(res);
+      setBoard(res.data.data.content);
+      setTotalPages(res.data.data.totalPages);
+      setCurrentPage(page);
+    } catch (error) {
+      console.error("Error: ", error);
+    }
+  };
+
+  // 페이지 변경
+  const handlePageChange = (newPage: number) => {
+    getBoard(newPage);
+  };
+
+  const renderPagination = () => {
+    const pages = Array.from({ length: totalPages }, (_, index) => index + 1);
+  
+    return (
+      <div className="pagination">
+        {pages.map((page) => (
+          <span
+            key={page}
+            className={page === currentPage ? "active" : ""}
+            onClick={() => handlePageChange(page)}
+          >
+            {page}
+          </span>
+        ))}
+      </div>
+    );
+  };
+
   return (
-    <>
-      {
-        board.map((board: BoardDto)=>
-          <Row>
-            <Col>{board.postId}</Col>
-            <Col><Link to={`/api/posts/${board.postId}`}>{board.postTitle}</Link></Col>
-            <Col>{board.postAuthor}</Col>
-            <Col>{board.createdAt}</Col>
-          </Row>)
-      }
-    </>
+    <Container>
+      <Table striped bordered hover responsive className="table">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th className="title-column">제목</th>
+            <th className="author-column">작성자</th>
+            <th className="like-column">좋아요</th>
+            <th className="unlike-column">싫어요</th>
+            <th className="createAt-column">작성일</th>
+          </tr>
+        </thead>
+        <tbody>
+          {board.map((board: BoardDto) => (
+            <tr key={board.postId}>
+              <td>{board.postId}</td>
+              <td>
+                <Link to={`/api/posts/${board.postId}`}>{board.postTitle}</Link>
+              </td>
+              <td>{board.postAuthor}</td>
+              <td>{board.postLike}</td>
+              <td>{board.postUnlike}</td>
+              <td>{board.createdAt}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+      
+    </Container>
   );
 };
 
--- src/pages/board/Post.tsx
+++ src/pages/board/Post.tsx
@@ -2,37 +2,77 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
 import { PostDto } from '../../dto/Post';
-import { Card } from 'react-bootstrap';
+
+import likeImage from '../../images/like_image.png';
+import unlikeImage from '../../images/unlike_image.png';
+
+import './Post.css';
 
 const Post: React.FC = () => {
     const { postId } = useParams();
-    console.log(postId);
     const [post, setPost] = useState<PostDto | null>(null);
+    const [likeCount, setLikeCount] = useState<number>(0);
+    const [unlikeCount, setUnlikeCount] = useState<number>(0);
 
     useEffect(() => {
         getPost(postId);
     }, [postId]);
 
     const getPost = async (id: string | undefined) => {
-        const res = await axios.get(`http://localhost:8080/api/posts/${postId}`);
-        console.log(res.data.data);
+        const res = await axios.get(`http://51.21.48.160:8080/api/posts/${postId}`);
         setPost(res.data.data);
+        setLikeCount(res.data.data.postLike);
+        setUnlikeCount(res.data.data.postUnlike);
+    };
+
+    // 좋아요(true) 또는 싫어요(false)
+    const isLike = async (isLike: boolean) => {
+        try {
+            const res = await axios.post(`http://51.21.48.160:8080/api/posts/${postId}/like?isLike=${isLike}`);
+
+            console.log(isLike);
+            if (isLike == true) {
+                setLikeCount(res.data.data.postLike);
+            } else {
+                setUnlikeCount(res.data.data.postUnlike);
+            }
+        } catch (error) {
+            console.error('Error:', error);
+        }
     };
 
     return (
         <>
             {post && (
-                <Card style={{ width: '1200px', height: '550px', margin: '30 auto 30 auto' }}>
-                    <Card.Body>
-                        <Card.Title>{post.postTitle}</Card.Title>
-                        <Card.Text>
-                            {post.postContent}
-                        </Card.Text>
-                        <button>좋아요</button>
-                        <button>싫어요</button>
-                        <button>신고하기</button>
-                    </Card.Body>
-                </Card>
+                <div className="post-container">
+                    <div className="post-content">
+                        <h2 className="post-header">{post.postTitle}</h2>
+                        <hr />
+                        <p className="mb-2 text-muted">
+                            {`${post.postAuthor} | 생성시간: ${post.createdAt} | 수정시간: ${post.modifiedAt}`}
+                        </p>
+                        <hr />
+                        <p>{post.postContent}</p>
+                    </div>
+                    <div className="post-actions">
+                        <div className="like-dislike">
+                            <img
+                                src={likeImage}
+                                alt="좋아요"
+                                onClick={() => isLike(true)}
+                            />
+                            <img
+                                src={unlikeImage}
+                                alt="싫어요"
+                                onClick={() => isLike(false)}
+                            />
+                        </div>
+                    </div>
+                    <div className="post-stats">
+                        <span className="post-stat">{post.postLike}</span>
+                        <span className="post-stat">{post.postUnlike}</span>
+                    </div>
+                </div>
             )}
         </>
     );
