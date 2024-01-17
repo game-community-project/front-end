18b5cd548284246332d5f6159bb13831611a629f
Begon <japgo@naver.com>
2024-01-17 16:16:03 +0900

    feat: admin 기능 구현 중.

--- src/pages/admin/Admin.tsx
+++ src/pages/admin/Admin.tsx
@@ -3,21 +3,86 @@ import { useNavigate } from "react-router-dom";
 import DatePicker from 'react-datepicker'
 import 'react-datepicker/dist/react-datepicker.module.css'
 import axios from "axios";
+import { useState } from "react";
+
+const headerHolder = {
+	access: '',
+	refresh: '',
+}
+
+function ButtonAdminLogin() {
+	const loginData = {
+		email: 'japgo@naver.com',
+		password: '1234',
+	}
+
+	const login = async () => {
+		try {
+			const loginUrl = 'http://localhost:8080/api/users/login';
+			const resp = await axios.post(loginUrl, loginData);
+			if (resp.status == 200) {
+				const access = resp.headers.access;
+				const refresh = resp.headers.refresh;
+
+				localStorage.setItem('access', access);
+				localStorage.setItem('refresh', refresh);
+				// headerHolder.access = access;
+				// headerHolder.refresh = refresh;
+			}
+			else {
+				alert("로그인 실패");
+			}
+		}
+		catch (error) {
+			console.error('exception : ', error);
+		}
+	}
+
+	return (
+		<div>
+			<Button onClick={login}>로그인</Button>
+		</div>
+	)
+}
 
 function Admin() {
 	return (
 		<Container>
-			<br/>
+			<br />
+			<ButtonAdminLogin />
+			<br />
 			<ButtonReportedPosts />
-			<br/>
+			<br />
 			<FormFindUser />
-			<br/>
+			<br />
 			<FormUserInfo />
 		</Container>
 	)
 }
 
 function FormFindUser() {
+	const [nickname, setNickname] = useState('');
+
+	const accessToken = localStorage.getItem('access');
+	const refreshToken = localStorage.getItem('refresh');
+
+	const find = async () => {
+		try {
+			const url = "http://localhost:8080/api/admin/users/" + nickname;
+			const resp = await axios.get(url, {
+				headers: {
+					access: accessToken,
+					refresh: refreshToken,
+				}
+			});
+
+			console.log( 'get user data : ', resp );
+		}
+		catch (error) {
+			console.error( 'find user error : ', error);
+		}
+	}
+
 	return (
 		<Container>
 			<Form>
@@ -26,10 +91,10 @@ function FormFindUser() {
 						<Form.Label>User Nickname</Form.Label>
 					</Col>
 					<Col>
-						<Form.Control type="text" placeholder="Enter User Nickname" />
+						<Form.Control type="text" placeholder="Enter User Nickname" onChange={(e) => setNickname(e.target.value)} />
 					</Col>
 					<Col>
-						<Button variant="primary" type="submit">Find</Button>
+						<Button variant="primary" type="submit" onClick={find}>Find</Button>
 					</Col>
 				</Row>
 			</Form>
@@ -79,7 +144,7 @@ function FormUserInfo() {
 
 					<Form.Group as={Col} controlId="formGridBlockDate">
 						<Form.Label>Block Date</Form.Label>
-						<DatePicker selected={new Date()} onChange={()=>{}} />
+						<DatePicker selected={new Date()} onChange={() => { }} />
 					</Form.Group>
 				</Row>
 
