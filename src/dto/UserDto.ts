c2cc42ce344b0d4e7d2ca4f700f988f38ac6c33c
Dahee Son <sxxdxh2@gmail.com>
2024-01-21 03:08:03 +0900

    feat : UserDto 수정

--- src/dto/UserDto.ts
+++ src/dto/UserDto.ts
@@ -1,5 +1,4 @@
 export interface UserDto {
-	id: bigint;
 	nickname: string;
 	introduction: string;
 	profile_url: string;
@@ -8,7 +7,6 @@ export interface UserDto {
   
 export interface GuestBookDto {
 	id: number;
-	//to_user_id: bigint;
 	content: string;
 	nickname: string;
 	createdAt: string;
