d303f0c5c719a929f8bff04f55031acba00b3e8c
Dahee Son <sxxdxh2@gmail.com>
2024-01-20 20:46:37 +0900

    feat : 페이징 추가

--- src/pages/guestbook/Guestbook.tsx
+++ src/pages/guestbook/Guestbook.tsx
@@ -4,6 +4,7 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { GuestBookDto } from '../../dto/UserDto';
 import './Guestbook.css';
+
 interface GuestbookProps {
     toUserId: number;
   }
